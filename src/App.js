4d33b7e671a93f6789b0707db876cc66cf998df7
jorge <jorge.baca@pucp.edu.pe>
2021-10-22 13:43:00 -0500

    posible solucion de animacion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 import RoomInfo from './components/RoomInfo';
 import Card from './components/Card';
 import Footer from './components/Footer';
 
 function App() {
+
+    const [fd, setFd] = useState(false);
+
     return (
         <div className="App">
         <header className="App-header">
@@ -13,7 +17,8 @@ function App() {
         </header>
         <RoomInfo cid={"ABCD1234"} nro-usuarios={4} />
         <p>Aqui van las cartillas</p>
-        <Card />
+        <Card faceDown={fd} />
+        <button onClick={() => {console.log("Wenas");setFd(!fd);}}> Presiona aqui </button>
         <p>Aqui las estadísticas</p>
         <Footer />
         </div>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,30 +1,31 @@
 import './Card.css';
 
-function Card() {
+function Card( props ) {
+
+    const { faceDown } = props;
 
     return (
         <div className="Card">
             <div className="carta-box">
                 <div className="carta">    
-                    <div className="cara detras">
-                    <img 
-                        src="https://w7.pngwing.com/pngs/902/280/png-transparent-ace-of-spades-playing-card-ace-of-hearts-spades-game-angle-king.png" 
-                        width="200" 
-                        height="250px" 
-                        alt=""
-                    />
+                    <div className="cara detras" style={ faceDown ? {transform: 'rotateY(180deg)'} : {transform: 'rotateY(0deg)' } }>
+                        <img 
+                            src="https://w7.pngwing.com/pngs/902/280/png-transparent-ace-of-spades-playing-card-ace-of-hearts-spades-game-angle-king.png" 
+                            width="200" 
+                            height="250px" 
+                            alt=""
+                        />
                     </div>
-                    <div className="cara">
-                    <img 
-                        src="http://chetart.com/blog/wp-content/uploads/2012/05/playing-card-back.jpg" 
-                        width="200" 
-                        height="250px" 
-                        alt=""
-                    />
+                    <div className="cara" style={ faceDown ? {transform: 'rotateY(0deg)'} : {transform: 'rotateY(180deg)'} }>
+                        <img 
+                            src="http://chetart.com/blog/wp-content/uploads/2012/05/playing-card-back.jpg" 
+                            width="200" 
+                            height="250px" 
+                            alt=""
+                        />
                     </div>    
                 </div>
             </div>
-            <button> Presiona aqui </button>
         </div>
     );
 }
